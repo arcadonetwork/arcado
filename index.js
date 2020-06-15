38e45b1186b46d52e1c3b616c911cb3f7ddc26a1
michielmulders <michielmulders1@gmail.com>
2020-06-15 20:10:41 +0200

    Add stop room transaction

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { Application, genesisBlockDevnet, configDevnet } = require('lisk-sdk');
 const CreateRoomTransaction = require('./transactions/create-room');
 const JoinRoomTransaction = require('./transactions/join-room')
+const StartRoomTransaction = require('./transactions/start-room')
+const StopRoomTransaction = require('./transactions/stop-room')
 
 configDevnet.app.label = 'arcado-network';
 configDevnet.modules.http_api.access.public = true;
@@ -8,6 +10,8 @@ configDevnet.modules.http_api.access.public = true;
 const app = new Application(genesisBlockDevnet, configDevnet);
 app.registerTransaction(CreateRoomTransaction);
 app.registerTransaction(JoinRoomTransaction);
+app.registerTransaction(StartRoomTransaction);
+app.registerTransaction(StopRoomTransaction);
 
 app
     .run()
