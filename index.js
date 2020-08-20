20239609cca4e1dbe84ff52bbd273ec171d4d767
Tom Ploem <tom.ploem@gmail.com>
2020-08-20 08:53:44 +0200

    lisk SDK version bump + transactions refactor

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const { Application, genesisBlockDevnet, configDevnet } = require('lisk-sdk');
 const CreateRoomTransaction = require('./transactions/create-room');
 const JoinRoomTransaction = require('./transactions/join-room')
@@ -7,11 +10,19 @@ const StopRoomTransaction = require('./transactions/stop-room')
 configDevnet.app.label = 'arcado-network';
 configDevnet.modules.http_api.access.public = true;
 
+const { ExtendedHTTPApiModule } = require('@moosty/lisk-extended-api');
+
 const app = new Application(genesisBlockDevnet, configDevnet);
 app.registerTransaction(CreateRoomTransaction);
 app.registerTransaction(JoinRoomTransaction);
 app.registerTransaction(StartRoomTransaction);
 app.registerTransaction(StopRoomTransaction);
+
+app.registerModule(ExtendedHTTPApiModule, {
+  port: 2020,
+  limit: 1000,
+  assets: ['gameId', 'roomId', 'recipientId']
+});
 
 app
     .run()
